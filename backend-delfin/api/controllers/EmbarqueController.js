8d2b49d5347f831b4a75d4ea1931e92d4e7edaa0
Brandon García <arcaniteamp@gmail.com>
2016-11-23 06:32:19 +0000

    cambios a retirarREmesa

--- backend-delfin/api/controllers/EmbarqueController.js
+++ backend-delfin/api/controllers/EmbarqueController.js
@@ -21,17 +21,7 @@ module.exports = {
 		var cantidad = req.param('cantidad');
 		var peso = req.param('peso');
 		Embarque.update({id: id}, {cantidadEmpaques: cantidad, peso: peso}).then(function (dataEmbarque) {
-			//Una vez editado el embarque, se crea una accion para el historial de ese embarque return res.json(data[0]);
-			Accion.create({
-				embarque: dataEmbarque[0].id,
-				tipo: 'retiro',
-				cantidad: cantidad,
-				pesoActual: dataEmbarque[0].peso
-			}).then(function(dataAccion) {
-				return res.json({accion: dataAccion, embarque: dataEmbarque[0]});
-			}).catch(function (err) {
-				return res.json(500, {err: err});
-			});
+			return res.json(dataEmbarque);
 		}).catch(function (err) {
 			return res.json(500, {err: err});
 		});
